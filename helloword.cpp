a718ab6ce0b4fe5a16ade49183ab6f09572df679
WilfridPurba <wilfridpurba@gmail.com>
2023-03-22 18:36:13 +0700

    Pertemuan Pertama Web Media

---
+++ helloword.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+cout<<"Halo Dunia"<<endl;
+cout<<"Nama Saya Wilfrid Lungguk Purba"<<endl;
+cout<<"Saya Mahasiswa Semester 4 di Universitas Mandiri Bina Prestasi"<<endl;
+cout<<"Saya berusia 21"<<endl;
+cout<<"Saya tinggal di Jl. Jamin Ginting Gg. Keluarga No.36"<<endl;
+cout<<"Saya memiliki Hobby Bermain Gitar"<<endl;
+cout<<"Saya sedang belajar bahasa pemrograman C++"<<endl;
+return 0;
+}
